76fecc5ab68329b8dfa813a3bae2ecfe24b3f7ea
Амиш Любимов <amish_lubimov@mail.ru>
2022-07-14 18:40:35 +0300

    fixed main image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
       <h1 className="text-2xl md:text-3xl lg:text-4xl font-normal">We create beautiful <br /> and fast web services</h1>
     </div>
     <div className="w-screen">
-      <img className="w-[96vw] xl:h-[46vh] mx-auto h-[18vh] md:h-[26vh] lg:h-[32vh] mt-6 rounded-3xl" src={mainImage} alt="Main Image" />
+      <img className="w-[96vw] 2xl:h-[44vh] 2xl:w-[88vw] xl:w-[90vw] xl:h-[36vh] mx-auto h-[14vh] md:h-[22vh] lg:h-[32vh] mt-6 rounded-3xl" src={mainImage} alt="Main Image" />
     </div>
     <div className="flex flex-col items-start mt-14 justify-center md:max-w-xl lg:max-w-2xl xl:max-w-5xl lg:px-32 xl:px-[17rem] px-14">
       <h1 className="text-2xl lg:text-4xl">Story, emotion <br /> and purpose</h1>
